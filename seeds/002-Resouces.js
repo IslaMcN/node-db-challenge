951b208375427d3104a4146279f8b0b8346d344b
Lambda_School_Loaner_212 <lambda_school_loaner_212@Lambda-School-Loaner-212s-MacBook-Pro.local>
2019-10-14 18:16:34 -0700

    index done

--- seeds/002-Resouces.js
+++ seeds/002-Resouces.js
@@ -1,10 +1,10 @@
 
 exports.seed = function(knex) {
   // Deletes ALL existing entries
-  return knex('table_name').del()
+  return knex('resource').del()
     .then(function () {
       // Inserts seed entries
-      return knex('table_name').insert([
+      return knex('resource').insert([
         {id: 1, Name: 'MDN', description: 'Super techninal guide to most if not all things with web dev'},
         {id: 2, Name: 'W3Schools', description: 'Very comprehensible and teaches you many things'},
         {id: 3, Name: 'StackOverFlow', description: 'DO NOT USE THIS RESOURCE DANGER DANGER! (but for real everyone uses it so its okay'}
